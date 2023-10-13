6bfbb6591858711f3bd345b415ce072e35425781
momocn <119989172+momocnz1@users.noreply.github.com>
2023-10-13 18:21:32 +0700

    9

--- calen.js
+++ calen.js
@@ -14,30 +14,27 @@ function save(val){
     datt.innerHTML ="Date: " + da;
     
 }
-const a1 = document.getElementById('a1')
-const a2 = document.getElementById('a2')
-const a19 = document.getElementById('a19')
-const a25 = document.getElementById('a25')
-const a4 = document.getElementById('a4')
-function sub(){
-    var ha = document.createElement('happy').innerHTML = "<img src='happy.png'width='18' height='18'/>";
-    a1.innerHTML = ha;
+function mood() {
+    
+    const selectedMood = clickedImage;
+    if (selectedMood) {
+        const currentDate = new Date();
+        const currentMonth = currentDate.toLocaleString('en-US', { month: 'long' });
+        const currentDay = currentDate.getDate();
+        const moodContainer = document.getElementById('moodContainer');
+        const moodElement = document.createElement('div');
+        moodElement.innerHTML = `<p>Mood: ${selectedMood}, Date: ${currentMonth} ${currentDay}</p>`;
+        moodContainer.appendChild(moodElement);
+    }
 }
-function da(){
-    var se = document.createElement('sad').innerHTML = "<img src='sad.png'width='18' height='18'/>";
-    a2.innerHTML = se;
-}
-function mo(){
-    var om = document.createElement('sad1').innerHTML = "<img src='sad (1).png'width='18' height='18'/>";
-    a19.innerHTML = om;
-}
-function cn(){
-    var nz = document.createElement('sceptic').innerHTML = "<img src='sceptic.png'width='18' height='18'/>";
-    a25.innerHTML = nz;
-}
-function sm(){
-    var le = document.createElement('smile').innerHTML = "<img src='happy1.png'width='18' height='18'/>";
-    a4.innerHTML = le;
+function changeMood(moodImage) {
+    const calendarDayElements = document.querySelectorAll('.calendar-day');
+    calendarDayElements.forEach((dayElement) => {
+
+        const image = document.createElement('img');
+        image.src = moodImage;
+        dayElement.innerHTML = '';
+        dayElement.appendChild(image);
+    });
 }
 
-    
